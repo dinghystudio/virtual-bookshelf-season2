692a21b7b8288353751b2d6f17cd4f34a9ff4562
Nils Borgböhmer <nils@dinghy.studio>
2024-11-11 14:45:50 +0100

    Rendering book component and adding todos
    Co-authored-by: Judith Böhlert <judicodes@users.noreply.github.com>

---
+++ components/books/Book.tsx
@@ -0,0 +1,40 @@
+import StarRating from "@/components/StarRating";
+
+export interface BookType {
+  id: number;
+  title: string;
+  author?: string;
+  publicationYear?: number;
+  description?: string;
+  personalNotes?: string;
+  rating?: number;
+}
+
+export function Book({ book }: { book: BookType }) {
+  const { title, author, publicationYear, description, personalNotes, rating } =
+    book;
+
+  return (
+    <li className="grid gap-2 content-start">
+      <header className="flex gap-2 items-center justify-between">
+        <p className="font-bold text-xl">{title}</p>
+      </header>
+      <p className="italic">
+        {author && <span>{author}</span>}
+        {publicationYear && <time>, {publicationYear}</time>}
+      </p>
+      {description && <p className="line-clamp-1">{description}</p>}
+      <article className="grid gap-1">
+        {personalNotes && (
+          <>
+            <h2 className="text-xs font-bold uppercase text-slate-500 mt-4">
+              Notes:
+            </h2>
+            <p className="line-clamp-1">{personalNotes}</p>
+          </>
+        )}
+        {rating && <StarRating filledStars={rating} />}
+      </article>
+    </li>
+  );
+}
